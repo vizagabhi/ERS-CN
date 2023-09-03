48b44e65117b4f06c2ea3141c5ef28ae554a27af
Abhishek Kumar <abhishekbokaro1998@gmail.com>
2023-09-03 20:34:18 +0530

    Whole setup.

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const url = process.env.MONGO_URI;
+mongoose.connect(url);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error connecting to the db'));
+
+db.once('open', function () {
+  console.log("Successfully connected to the Database");
+});
+
+module.exports = db;
